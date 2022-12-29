e054314632bf4f1726610a576be139697f0102a3
Martin Cervantes <martin_cervantes@epam.com>
2022-12-29 16:38:08 -0600

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Search from './components/Search';
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handlesearchSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +18,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
